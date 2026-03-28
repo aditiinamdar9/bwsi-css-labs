a513fa833ae0b74c7443623e295333a973fb52da
Aditi <you@example.com>
2026-03-28 10:05:03 -0400

    step 2: adding intro

--- labs/lab_1/lab_1a.py
+++ labs/lab_1/lab_1a.py
@@ -11,6 +11,9 @@ def main():
     name = "Aditi Inamdar" # TODO: Insert your name between the double quotes
 
     print(f"{name}, Welcome to the CSS course!")
+    print(f"{name}, Hey everyone! My name is Aditi Inamdar, and I'm a sophomore at Deep Run High School in the Center for Information Technology. I am extremely interested in how AI is transforming all computer science fields, and how that changed can be utilized to the engineers' benefit. 
+Outside of CS, I participate in Debate, Model United Nations, and Technology Student Associations. I love to teach students public speaking skills and help them build their own perspectives on global issues (which I accomplish via MUN). I am also an avid reader (I don't have a specific favorite genre; I try to expose myself to everything). I also love music and have been trained vocally since I was 5 years old. 
+I can't wait to learn more about cyber spaces and expand my knowledge about this field!")
 
 if __name__ == "__main__":
     main()
