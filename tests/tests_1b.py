c59074e7ee88c794beddfcfbd9479868fd556ab3
Aditi <you@example.com>
2026-03-28 11:02:22 -0400

    step 5 run test

--- tests/tests_1b.py
+++ tests/tests_1b.py
@@ -12,7 +12,7 @@ def test_addition():
     assert simple_calculator("add", -2, 2) == 0         # Test for negative and positive number
     assert simple_calculator("add", 0, 0) == 0          # Test for zero addition
 
-def test_subtraction():
+def test_subytraction():
     assert simple_calculator("subtract", 5, 3) == 2     # Test for positive numbers
     assert simple_calculator("subtract", -2, -2) == 0   # Test for negative numbers
     assert simple_calculator("subtract", 0, 5) == -5    # Test for zero minuend
